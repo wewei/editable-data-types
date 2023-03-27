1f4cb75397dfd287134840f3893df496d36660f5
Wei Wei <shazhouke@gmail.com>
2023-03-27 19:04:08 +0800

    Use TreeIx instead of [Int] for tree indices

--- src/Editable/Tree/InsDelMov.hs
+++ src/Editable/Tree/InsDelMov.hs
@@ -5,12 +5,34 @@
 module Editable.Tree.InsDelMov where
 
 import Data.Tree ( Tree(..), Forest )
-import Editable.Core (Invertable (invert), Editable (apply))
+import Editable.Core (Invertable (invert), Editable (apply, (~)), Rebasable(rebase), batchRebase)
+import Data.Maybe (fromJust)
+
+newtype TreeIx = TreeIx [Int] deriving (Ord, Eq, Show)
+
+(~>) :: TreeIx -> TreeIx -> Bool
+(TreeIx ns) ~> (TreeIx ms) = take (length ms) ns == ms
+
+parent :: TreeIx -> TreeIx
+parent (TreeIx []) = TreeIx []
+parent (TreeIx ns) = TreeIx . reverse . drop 1 . reverse $ ns
+
+root :: TreeIx
+root = TreeIx []
+
+instance Editable (Tree a) TreeIx where
+    apply :: TreeIx -> Tree a -> Maybe (Tree a)
+    apply (TreeIx []) t = Just t
+    apply (TreeIx (n:ns)) (Node v ch)
+        | n < 0     = Nothing
+        | otherwise = case splitAt n ch of
+            (_, c:_) -> apply (TreeIx ns) c
+            _        -> Nothing
 
 data InsDelMov a
-    = Insert [Int] (Tree a)
-    | Delete [Int] (Tree a)
-    | Move   [Int] [Int]
+    = Insert TreeIx (Tree a)
+    | Delete TreeIx (Tree a)
+    | Move   TreeIx TreeIx
     | NoChange
 
 deriving instance Eq a => Eq (InsDelMov a)
@@ -24,37 +46,66 @@ instance Invertable (InsDelMov a) where
     invert (Delete ns t) = Insert ns t
     invert (Move ns ms)  = Move ms ns
 
-splitTree :: [Int] -> Tree a -> Maybe (Tree a, Tree a)
-splitTree [] _        = Nothing
-splitTree [n] (Node root sub)
-    | n < 0           = Nothing
-    | n >= length sub = Nothing
-    | otherwise       = let
-        (xs, y:ys) = splitAt n sub
-        in Just (Node root (xs <> ys), y)
-splitTree (n:ns) (Node root sub)
-    | n < 0           = Nothing
-    | n >= length sub = Nothing
-    | otherwise       = do
-        let (xs, y:ys) = splitAt n sub
-        (y', z) <- splitTree ns y
-        return (Node root (xs <> (y':ys)), z)
+splitTree :: TreeIx -> Tree a -> Maybe (Tree a, Tree a)
+splitTree (TreeIx []) _  = Nothing
+splitTree (TreeIx [n]) (Node v ch)
+    | n < 0             = Nothing
+    | n >= length ch    = Nothing
+    | otherwise         = let
+        (xs, y:ys) = splitAt n ch
+        in Just (Node v (xs <> ys), y)
+splitTree (TreeIx (n:ns)) (Node v ch)
+    | n < 0             = Nothing
+    | n >= length ch    = Nothing
+    | otherwise         = do
+        let (xs, y:ys) = splitAt n ch
+        (y', z) <- splitTree (TreeIx ns) y
+        return (Node v (xs <> (y':ys)), z)
 
-joinTree :: [Int] -> Tree a -> Tree a -> Maybe (Tree a)
-joinTree [] _ _ = Nothing
-joinTree [n] t (Node root sub)
-    | n < 0          = Nothing
-    | n > length sub = Nothing
-    | otherwise      = let
-        (xs, ys) = splitAt n sub
-        in Just $ Node root (xs <> (t:ys))
-joinTree (n:ns) t (Node root sub)
-    | n < 0          = Nothing
-    | n < length sub = do
-        let (xs, y:ys) = splitAt n sub
-        y' <- joinTree ns t y
-        return $ Node root (xs <> (y':ys))
-    | otherwise      = Nothing
+joinTree :: TreeIx -> Tree a -> Tree a -> Maybe (Tree a)
+joinTree (TreeIx []) _ _ = Nothing
+joinTree (TreeIx [n]) t (Node v ch)
+    | n < 0         = Nothing
+    | n > length ch = Nothing
+    | otherwise     = let
+        (xs, ys) = splitAt n ch
+        in Just $ Node v (xs <> (t:ys))
+joinTree (TreeIx (n:ns)) t (Node v ch)
+    | n < 0         = Nothing
+    | n < length ch = do
+        let (xs, y:ys) = splitAt n ch
+        y' <- joinTree (TreeIx ns) t y
+        return $ Node v (xs <> (y':ys))
+    | otherwise     = Nothing
+
+instance Editable TreeIx (InsDelMov a) where
+    apply :: InsDelMov a -> TreeIx -> Maybe TreeIx
+    apply _ (TreeIx []) = Just root
+    apply NoChange ix = Just ix
+    apply (Insert (TreeIx []) _) _ = Nothing
+    apply (Insert (TreeIx [n]) _) ix@(TreeIx (m:ms))
+        | n < 0     = Nothing
+        | n > m     = Just ix
+        | otherwise = Just . TreeIx $ (m + 1) : ms
+    apply (Insert (TreeIx (n:ns)) t) ix@(TreeIx (m:ms))
+        | n < 0     = Nothing
+        | n /= m    = Just ix
+        | otherwise = do
+            TreeIx ms' <- apply (Insert (TreeIx ns) t) (TreeIx ms)
+            return . TreeIx $ n:ms'
+    apply (Delete (TreeIx []) _) _ = Nothing
+    apply (Delete (TreeIx [n]) _) ix@(TreeIx (m:ms))
+        | n < 0     = Nothing
+        | n > m     = Just ix
+        | n == m    = Just root
+        | otherwise = Just . TreeIx $ (m - 1) : ms
+    apply (Delete (TreeIx (n:ns)) _) ix@(TreeIx (m:ms))
+        | n < 0     = Nothing
+        | n /= m    = Just ix
+        | otherwise = do
+            TreeIx ms' <- apply (Delete (TreeIx ns) (Node () [])) (TreeIx ms)
+            return . TreeIx $ if null ms' then [] else n:ms'
+
 
 adjustInsertion :: [Int] -> [Int] -> [Int]
 adjustInsertion [] ms = ms
@@ -72,13 +123,15 @@ adjustDeletion _ []  = []
 adjustDeletion [n] (m:ms)
     | n < m     = (m - 1) : ms
     | n > m     = m : ms
-    | otherwise = []
+    | otherwise = [n]
 adjustDeletion (n:ns) (m:ms)
-    | n == m    = let
-        ms' = adjustDeletion ns ms
-        in if null ms' then [] else n:ms'
+    | n == m    = m : adjustDeletion ns ms
     | otherwise = m : ms
 
+next :: [Int] -> [Int]
+next []     = []
+next [n]    = [n + 1]
+next (n:ns) = n:next ns
 
 instance Eq a => Editable (Tree a) (InsDelMov a) where
     apply :: InsDelMov a -> Tree a -> Maybe (Tree a)
@@ -87,9 +140,39 @@ instance Eq a => Editable (Tree a) (InsDelMov a) where
     apply (Delete ns t) s = do
         (s', t') <- splitTree ns s
         if t' == t then Just s' else Nothing
-    apply (Move ns ms) s  = let
-        ms' = adjustDeletion ns ms
-        in
-            if null ms' then Nothing else do
-                (s', t) <- splitTree ns s
-                joinTree ms' t s'
+    apply (Move ns ms) s  = do
+        ms' <- Just ms ~ Delete ns (Node () [])
+        if ms' == root then Nothing else do
+            (s', t) <- splitTree ns s
+            joinTree ms' t s'
+
+(~=) :: [Int] -> [Int] -> Bool
+ns ~= ms = take (length ms) ns == ms
+
+-- instance Ord a => Rebasable (InsDelMov a) where
+--     rebase :: Ord a => InsDelMov a -> InsDelMov a -> Maybe (InsDelMov a, InsDelMov a)
+--     rebase NoChange x = Just (NoChange, x)
+--     rebase x NoChange = Just (x, NoChange)
+--     rebase (Insert ns1 t1) (Insert ns2 t2)
+--         | ns1 /= ns2  = Just
+--             ( Insert (adjustInsertion ns2 ns1) t1
+--             , Insert (adjustInsertion ns1 ns2) t2)
+--         | t1 < t2     = Just
+--             ( Insert ns1 t1
+--             , Insert (next ns2) t2)
+--         | t1 > t2     = Just
+--             ( Insert (next ns1) t1
+--             , Insert ns2 t2)
+--         | otherwise   = Just (NoChange, NoChange)
+--     rebase (Insert ns1 t1) (Delete ns2 t2)
+--         | ns1 ~= ns2  = Just
+--             ( NoChange
+--             , Delete ns2 (fromJust (joinTree (drop (length ns2) ns1) t1 t2)))
+--         | ns1 /= ns2  = Just
+--             ( Insert (adjustDeletion ns2 ns1) t1
+--             , Delete (adjustInsertion ns1 ns2) t2)
+--         | otherwise   = Just
+--             ( Insert ns1 t1
+--             , Delete (next ns2) t2)
+--     -- rebase (Insert ns1 t1) (Move ns2 ms)
+--     --     | 
